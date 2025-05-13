e5f4e50e249bc26aa047aab61540af1f07eaf9cc
Mchcc-00 <Bleachowl98@gmail.com>
2025-05-13 07:54:30 -0500

    actualización base de datos

---
+++ usuarios/config.php
@@ -0,0 +1,9 @@
+<?php
+// config.php
+
+define('DB_SERVER', 'localhost');   // Servidor de BD (usualmente localhost)
+define('DB_USERNAME', 'root');      // usuario de MySQL
+define('DB_PASSWORD', '');          //contraseña de MySQL (dejar vacía si no tiene)
+define('DB_NAME', 'lodgehub');   // nombre de base de datos
+
+?>
