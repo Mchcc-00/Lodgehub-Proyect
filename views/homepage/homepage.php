3ca4069ec5b3a27b567ddb1b6fd706b3ef40e353
BrayanPulido06 <brayan06.pulido@gmail.com>
2025-07-03 11:18:37 -0500

    login modificado

---
+++ views/homepage/homepage.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link rel="stylesheet" href="../../public/assets/css/styles.css"> <!-- Enlaza el archivo CSS -->
+
+</head>
+<body>
+
+        <div class="navbar">
+        <div class="left">
+            <img src="../public/img/FLECHA.png" alt="Atras" id="flechaNav">
+            <h1>LODGEHUB</h1>
+        </div>
+        <div class="right">
+            <img src="../public/img/iconoPerfil.png" alt="Perfil" id="IconUserNav">
+        </div>
+    </div>
+
+    <div class="container">
+        <aside>
+            <div class="Lateral">
+                <div class="ListaElementos">
+                    <ul>
+                        <li>
+                            <a href="indexReservasmain.php" class="paginaActual">RESERVAS</a>
+                        </li>
+                        <li>
+                            <a href="indexHabitaciones.html" class="otrasPaginas">HABITACIONES</a>
+                        </li>
+                        <li>
+                            <a href="indexMantenimiento.html" class="otrasPaginas">MANTENIMIENTO</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="linea-horizontalSeparadorIconos"></div>
+                    <div class="ListaElementosIconos">
+                        <div class="iconoPQRS">
+                            <img src="../public/img/iconoPQRS.png" alt="PQRS" id="IconPQRS">
+                        </div>
+                        <div class="iconoAjustes">
+                            <img src="../public/img/tuercaAjustes.png" alt="Ajustes" id="IconAjustes">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </aside>
+
+
+</body>
+</html>
