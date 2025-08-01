985bad15b158b34ce8590aa037384632159f0170
Mchcc-00 <162225949+Mchcc-00@users.noreply.github.com>
2025-08-01 10:24:09 -0500

    Actualizar index.php

--- public/index.php
+++ public/index.php
@@ -34,12 +34,5 @@ $router->add('/usuarios/editar', 'UsuarioController', 'mostrarFormularioEdicion'
 $router->add('/usuarios/actualizar', 'UsuarioController', 'actualizar', 'POST');
 $router->add('/usuarios/eliminar', 'UsuarioController', 'eliminar', 'GET');
 
-echo "<b>URI que se intenta buscar:</b>";
-var_dump($uri);
-
-echo "<br><br><b>Rutas disponibles en el mapa:</b>";
-var_dump($router->routes);
-
-die();
 //Poner a Trabajar al Router
 $router->dispatch($uri, $request_method);
