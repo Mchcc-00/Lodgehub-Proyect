044694a2d85f2b4300633b1dc32d455b9d9a0cc5
BrayanPulido06 <brayan06.pulido@gmail.com>
2025-08-01 19:28:59 -0500

    homepage con boostrap

--- app/views/layouts/nav.php
+++ app/views/layouts/nav.php
@@ -1,20 +1,31 @@
-
-<div class="main-container">
-
-        <header class="top-bar">
-            <div class="top-bar-left">
-                <img src="../../../public/assets/img/LogoClaroLH.png" alt="Logo Lodgehub" class="top-logo">
-            </div>
-
-        <div>
-            <?php include '../login/navbar.php'?>
-        </div>
-
-            <div class="top-bar-right">
-                <i class="fas fa-user-circle user-icon" title="Perfil Usuario"></i>
-            </div>
-
-        </header>
-
-
-
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../homepage/cerrarSesion.php">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
