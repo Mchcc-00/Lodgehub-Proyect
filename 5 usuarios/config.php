1f66cab14338e1b0e33eb7270a86b2988b137a2b
Daniel10bdhn <danielfrmartinez2005@gmail.com>
2025-08-04 07:59:07 -0500

    eliminación carpetas

--- 5 usuarios/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php
-
-define('DB_SERVER', 'localhost');   // Servidor de BD (usualmente localhost)
-define('DB_USERNAME', 'root');      // usuario de MySQL
-define('DB_PASSWORD', '');          //contraseña de MySQL (dejar vacía si no tiene)
-define('DB_NAME', 'lodgehub');   // nombre de base de datos
-
-?>
