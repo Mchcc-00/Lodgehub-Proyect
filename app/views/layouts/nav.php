39e6c229c9e5f962c2cfe4674d1668dd3b06b4d9
JonathanLopez85 <jm672439@gmail.com>
2025-08-03 22:12:16 -0500

    Se implemento el nav pero aun falta

--- app/views/layouts/nav.php
+++ app/views/layouts/nav.php
@@ -13,7 +13,7 @@
             <li><a class="dropdown-item" href="../homepage/homepage.php">Home</a></li>
             <li><a class="dropdown-item" href="../../../6_Reservas/2R/mainReservas.php">Reservas</a></li>
             <li><a class="dropdown-item" href="../../../HABITACIONES/views/Habitaciones/dashboardHab.php">Habitaciones</a></li>
-            <li><a class="dropdown-item" href="../../../MANTENIMIENTO/views/dashboard.php">Mantenimiento</a></li>
+            <li><a class="dropdown-item" href="../../../app//views/Habitaciones/dashboard.php">Mantenimiento</a></li>
             <li><a class="dropdown-item" href="../../../PQRS/views/dashboard.php">PQRS</a></li>
             <li><a class="dropdown-item" href="../../../app/views/Usuarios/lista.php">Usuarios</a></li>
           </ul>
