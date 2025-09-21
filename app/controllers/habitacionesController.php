df6edb56aeee310b468cc92876581dd74d0fe683
BrayanPulido06 <brayan06.pulido@gmail.com>
2025-09-21 15:23:29 -0500

    habitaciones

--- app/controllers/habitacionesController.php
+++ app/controllers/habitacionesController.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . '../models/habitacionesModel.php';
+require_once __DIR__ . '/../models/habitacionesModel.php';
 
 class HabitacionesController {
     private $modelo;
