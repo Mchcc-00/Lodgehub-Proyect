5e8335c17e014815c819d1e1abab6110f9858d6c
BrayanPulido06 <brayan06.pulido@gmail.com>
2025-09-21 16:32:48 -0500

    nuevo codigo

--- app/views/listaHabitaciones.php
+++ app/views/listaHabitaciones.php
@@ -8,7 +8,7 @@ $currentPage = 'Habitaciones'; // Para activar el item en el sidebar
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestión de Habitaciones - LodgeHub</title>
+    <title>Gestión de Habitacioness - LodgeHub</title>
     <!-- Dependencias -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
